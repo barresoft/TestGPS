19607208cb722689dcd94d40f6e5380354a36972
barresoft <cristianbarreto@gmail.com>
2014-12-26 21:01:25 -0200

    Commit

--- app/src/main/java/com/barresoft/testgps/MainActivity.java
+++ app/src/main/java/com/barresoft/testgps/MainActivity.java
@@ -6,11 +6,14 @@ import android.hardware.Camera.Parameters;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Chronometer;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +22,11 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity implements LocationListener {
 
     private boolean led=false;
+    private Camera camera = Camera.open();
+    private Parameters cameraParameters = camera.getParameters();
+    private double speed=0;
+    private Chronometer cronometro;
+    private boolean cronometroHabilitado=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +34,27 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
         setContentView(R.layout.activity_main);
 
         iniciarGPS();
+
+        cronometro = (Chronometer)findViewById(R.id.cronometro);
     }
 
     private void iniciarGPS(){
+        //Enable GPS
+        Intent intent = new Intent("android.location.GPS_ENABLED_CHANGE");
+        intent.putExtra("enabled", true);
+        context.sendBroadcast(intent);
+
+        //Disable GPS
+        Intent intent = new Intent("android.location.GPS_ENABLED_CHANGE");
+        intent.putExtra("enabled", false);
+        context.sendBroadcast(intent);
+
+
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         lm.requestLocationUpdates( LocationManager.GPS_PROVIDER,
                 1000,   // 1 segundo
                 10, this);
+
     }
 
     @Override
@@ -59,7 +81,7 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
 
     @Override
     public void onLocationChanged(Location location) {
-        double speed = location.getSpeed(); //m/s
+        speed = location.getSpeed(); //m/s
         speed=speed*3.6; //km/h
         SeekBar sbarVelocidad = (SeekBar)findViewById(R.id.sbarVelocidad);
         int speedMax = sbarVelocidad.getMax();
@@ -71,6 +93,15 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
             sbarVelocidad.setMax((int) speed);
         }
         sbarVelocidad.setProgress((int) speed);
+
+        if (speed>0 && cronometroHabilitado==false){
+            cronometroHabilitado=true;
+            cronometro.start();
+            //long time = SystemClock.elapsedRealtime()-cronometro.getBase();
+            //t.setBase(SystemClock.elapsedRealtime());
+            //t.start();
+        }
+
     }
 
     @Override
@@ -89,38 +120,30 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
     }
 
     private void LED(){ //SWITCH
-        Camera camera = Camera.open();
-        Parameters p = camera.getParameters();
         if (led==false) {
             led=true;
-            p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-            camera.setParameters(p);
+            cameraParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(cameraParameters);
             camera.startPreview();
         }else{
             led=false;
-            p.setFlashMode(Parameters.FLASH_MODE_OFF);
-            camera.setParameters(p);
+            cameraParameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+            camera.setParameters(cameraParameters);
             camera.stopPreview();
-            camera.release();
-            camera = null;
         }
     }
 
     private void LED(Boolean bool){
-        Camera camera = Camera.open();
-        Parameters p = camera.getParameters();
         if (bool){
             led=true;
-            p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-            camera.setParameters(p);
+            cameraParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(cameraParameters);
             camera.startPreview();
         }else{
             led=false;
-            p.setFlashMode(Parameters.FLASH_MODE_OFF);
-            camera.setParameters(p);
+            cameraParameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+            camera.setParameters(cameraParameters);
             camera.stopPreview();
-            camera.release();
-            camera = null;
         }
     }
 
@@ -130,6 +153,5 @@ public class MainActivity extends ActionBarActivity implements LocationListener 
 
     public void ledSwitch(View view){
         LED();
-        msg("led?");
     }
 }
